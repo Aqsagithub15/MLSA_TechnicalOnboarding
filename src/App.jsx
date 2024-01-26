c5fc47fa8f7d93a65a161f833fb0899b7619ded7
Aqsa Tariq <aqsat7499@gmail.com>
2024-01-27 01:39:32 +0500

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Aqsa Tariq",
+  title: "UI/UX Designer",
+  email: "aqsa.tariq@studentambassadors.com",
+  gitHub: "Aqsagithub15",
+  instagram: "aqsatariq2002",
+  linkedIn: "aqsatariq2002",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
